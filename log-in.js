1a4af53dbeaf33a0d35f97e7c2da46301d449beb
BLAU BRIGITTE <49439094@ort>
2024-08-14 11:32:01 -0300

    nada wow

--- log-in.js
+++ log-in.js
@@ -1,39 +1,56 @@
+
 document.addEventListener("DOMContentLoaded", function() {
     function showTab(tabId) {
-
         let tabContent = document.getElementsByClassName("content");
         for (let i = 0; i < tabContent.length; i++) {
             tabContent[i].classList.remove("active");
         }
         let tab = document.getElementById(tabId);
         if (tab) {
-            tab.classList.add("active");}
-
+            tab.classList.add("active");
+        }
         let tabLinks = document.getElementsByClassName("welcome-button");
         for (let i = 0; i < tabLinks.length; i++) {
-            tabLinks[i].classList.remove("active"); }
+            tabLinks[i].classList.remove("active");
+        }
+
         let tabLink = document.querySelector(`.welcome-button[href$='#${tabId}']`);
         if (tabLink) {
             tabLink.classList.add("active");
         }
     }
-  let hash = window.location.hash.substring(1); 
+    function handleButtonClick(event) {
+        let tabId = event.currentTarget.id.replace('button-', '');
+        showTab(tabId);
+    }
+    let buttons = document.getElementsByClassName("decision-button");
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", handleButtonClick);
+    }
+    let hash = window.location.hash.substring(1);
     if (hash) {
         showTab(hash);
     }
 });
 
-  let buttonLogin = getElementById("button-login").addEventListener("click")
 
-//backk 
-let submit = document.getElementById("submit");
+let submit = document.getElementById("submit-login");
 submit.addEventListener("click", check_user);
-let password = document.getElementById("password");
-let user = document.getElementById("username");
 async function check_user(){
+    let user= {
+        "USER": document.getElementById("username").value,
+        "PASSWORD": document.getElementById("password").value
+    }
+    
+    
+
     console.log("Hola");
-    console.log(user, password);
-    let response = await fetch(`https://secure-track-db.vercel.app/userLogin?USER=${user.value}&PASSWORD=${password.value}`, {});
+    console.log(user);
+    let response = await fetch(`https://secure-track-db.vercel.app/users/login`, {
+        method:"POST",
+        body:user
+    });
     let data = await response.json();
     console.log(data);
+    location.href= "index.html"
 }
